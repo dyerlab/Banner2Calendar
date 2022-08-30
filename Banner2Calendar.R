ed3e4b138eddd9e3f2ba67d3486734c3a91f8c0e
dyerlab <dyerlab@icloud.com>
2022-08-30 13:55:36 -0400

    finished first push

--- Banner2Calendar.R
+++ Banner2Calendar.R
@@ -3,6 +3,9 @@
 library( tidyverse )
 library( lubridate )
 
+
+FirstMonday <- mdy("01-23-2023")
+
 read_csv("2023.csv") |>
   mutate( Location = paste( BLD, ROOM) ) |>
   mutate( Location = ifelse( is.na(ROOM), NA, Location) ) -> tmp 
@@ -16,38 +19,98 @@ data.frame( Subject = NA,
             Location = NA
             ) -> schedule
 
+
+
+makeTime <- function( raw ) { 
+  tm <- as.numeric( raw ) 
+  am.pm <- ifelse( tm > 1200, " PM"," AM")
+  corr <- ifelse( am.pm == " PM", 12, 0 )
+  ret <- c( floor(tm / 100) - corr,
+            ":",
+            str_sub(raw,-2),
+            am.pm )
+  return( paste( ret, collapse = "" ) )
+}
+
+
+
+
+
 for( i in 1:nrow(tmp) ) { 
   
-  start <- as.numeric(tmp$START[i])
+  start <- tmp$START[i]
   
-  
-  # Classes with times have start time
   if( !is.na(start) ) {
-    end <- as.numeric(tmp$END[i])
-    apstart <- ifelse( start > 1200, "PM", "AM" )
     
-    if( apstart == "PM" ) { 
-      start <- start - 1200
-    }
-    start <- start/100
-    
-    
-    apend <- ifelse( end > 1200, "PM","AM")
-    if( apend  == "PM" ) { 
-      end <- end - 1200
-    }
-    end <- end/100
-    
-    description <- tmp$TITLE[i]
+    stime <- makeTime( start )
+    etime <- makeTime( tmp$END[i] )
+    description <- paste( tmp$COURSE[i], tmp$TITLE[i], sep=": ")
     instructor <- tmp$PROFESSOR[i]
     location <- tmp$Location[i]
     
+    if( !is.na( tmp$MON[i]) ) { 
+      schedule <- rbind( schedule, 
+                         data.frame( Subject = description,
+                                     `Start Date` = as.character(FirstMonday),
+                                     `End Date` = as.character(FirstMonday),
+                                     `Start Time` = stime,
+                                     `End Time` = etime,
+                                     Description = instructor,
+                                     Location = location )
+                         )
+    }
     
-    if( !.is.na( tmp$MON[i]) ) { 
-      
+    if( !is.na( tmp$TUE[i]) ) { 
+      schedule <- rbind( schedule, 
+                         data.frame( Subject = description,
+                                     `Start Date` = as.character(FirstMonday + 1),
+                                     `End Date` = as.character(FirstMonday + 1),
+                                     `Start Time` = stime,
+                                     `End Time` = etime,
+                                     Description = instructor,
+                                     Location = location )
+      )
+    }
+    
+    if( !is.na( tmp$WED[i]) ) { 
+      schedule <- rbind( schedule, 
+                         data.frame( Subject = description,
+                                     `Start Date` = as.character(FirstMonday + 2),
+                                     `End Date` = as.character(FirstMonday + 2),
+                                     `Start Time` = stime,
+                                     `End Time` = etime,
+                                     Description = instructor,
+                                     Location = location )
+      )
+    }
+    
+    if( !is.na( tmp$THU[i]) ) { 
+      schedule <- rbind( schedule, 
+                         data.frame( Subject = description,
+                                     `Start Date` = as.character(FirstMonday + 3),
+                                     `End Date` = as.character(FirstMonday + 3),
+                                     `Start Time` = stime,
+                                     `End Time` = etime,
+                                     Description = instructor,
+                                     Location = location )
+      )
+    }
+    
+    if( !is.na( tmp$FRI[i]) ) { 
+      schedule <- rbind( schedule, 
+                         data.frame( Subject = description,
+                                     `Start Date` = as.character(FirstMonday + 4),
+                                     `End Date` = as.character(FirstMonday + 4),
+                                     `Start Time` = stime,
+                                     `End Time` = etime,
+                                     Description = instructor,
+                                     Location = location )
+      )
     }
     
     
+    
+        
     
   }
 
@@ -55,6 +118,8 @@ for( i in 1:nrow(tmp) ) {
   
 }
 
+schedule |>
+  filter( !is.na(Subject) ) -> schedule
 
 View(schedule)
 
